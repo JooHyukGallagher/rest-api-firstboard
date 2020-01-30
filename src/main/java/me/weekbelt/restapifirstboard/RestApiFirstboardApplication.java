c4feac8e66b722063f49437e2e4de88beaa7db1e
joohyuk <vfrvfr4207@gmail.com>
2020-01-30 13:00:10 +0900

    프로젝트 생성

---
+++ src/main/java/me/weekbelt/restapifirstboard/RestApiFirstboardApplication.java
@@ -0,0 +1,13 @@
+package me.weekbelt.restapifirstboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiFirstboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiFirstboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/weekbelt/restapifirstboard/RestApiFirstboardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.restapifirstboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiFirstboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
